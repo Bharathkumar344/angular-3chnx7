08fea7ba3468626879a613461992b177f29cf342
Bharathkumar344 <bharathtinku344@gmail.com>
2022-03-03 16:42:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-3chnx7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-3chnx7)
---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+ navbar=['Home','Features', 'Pricing', 'FAQs', 'About'];
+ matter=
+ 'Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.';
+}
